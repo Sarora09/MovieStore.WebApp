7afd1975f9d5fc31699cc677d1fa9e181b0f895e
Sarora09 <52610194+Sarora09@users.noreply.github.com>
2022-04-08 20:12:11 -0600

    Added the frontend validations in the UpdateMovie component

--- src/components/UpdateMovie.js
+++ src/components/UpdateMovie.js
@@ -11,6 +11,7 @@ const UpdateMovie = () => {
     const [ratingErr, setRatingErr] = useState("");
     const [genreErr, setGenreErr] = useState("");
     const [rentErr, setRentErr] = useState("");
+    const [serverErr, setServerErr] = useState("");
     const [updateResult, setUpdateResult] = useState("");
     const params = useParams();
 
@@ -23,18 +24,24 @@ const UpdateMovie = () => {
                 authorization: `Bearer ${token}`
             }
         });
-        var movie = await movieResult.json();
-        // console.log(movie);
-        if (movie) {
-            setMovieName(movie.name);
-            setRating(movie.rating);
-            setGenre(movie.genre);
-            setRent(movie.rentPrice);
+        if (movieResult.status == 200) {
+            var movie = await movieResult.json();
+            if (movie) {
+                setMovieName(movie.name);
+                setRating(movie.rating);
+                setGenre(movie.genre);
+                setRent(movie.rentPrice);
+            }
         }
+        else if (movieResult.status == 404) {
+            setServerErr("Selected movie does not exist in the database. Please contact the server side administrator for details.");
+        }
+
     }, []);
 
     // To clear the validation errors with state change once the user provided the value for the state
     useEffect(() => {
+        setServerErr("");
         if (name) {
             setMovieNameErr("");
         }
@@ -51,43 +58,70 @@ const UpdateMovie = () => {
 
     const submitMovie = async (e) => {
         e.preventDefault();
-        var id = params.id;
-        var token = localStorage.getItem('token');
-        var movieResult = await fetch(`https://movie-collection-api-app.azurewebsites.net/api/movies/${id}`, {
-            method: "put",
-            body: JSON.stringify({ id, name, rating, genre, rentPrice }),
-            headers: {
-                authorization: `Bearer ${token}`,
-                'Content-Type': 'application/json'
+        try {
+            if (name && rating && genre && rentPrice) {
+                var id = params.id;
+                var token = localStorage.getItem('token');
+                var movieResult = await fetch(`https://movie-collection-api-app.azurewebsites.net/api/movies/${id}`, {
+                    method: "put",
+                    body: JSON.stringify({ id, name, rating, genre, rentPrice }),
+                    headers: {
+                        authorization: `Bearer ${token}`,
+                        'Content-Type': 'application/json'
+                    }
+                });
+                if (movieResult.status == 200) {
+                    setUpdateResult("Movie record updated");
+                }
+                else if (movieResult.status == 400) {
+                    var errorObject = await movieResult.json();
+                    if (errorObject.errors.Name) {
+                        setMovieNameErr("Movie Name is required.");
+                    }
+                    if (errorObject.errors.rating) {
+                        setRatingErr("Please rate between 1-10 for this movie.");
+                    }
+                    if (errorObject.errors.rentPrice) {
+                        setRentErr("Rent is required.");
+                    }
+                    if (errorObject.errors.Genre) {
+                        setGenreErr("Genre is required.");
+                    }
+                }
+                else if (movieResult.status == 404) {
+                    setServerErr("Selected movie do not exist in the database. Please contact the server side administrator for details.");
+                }
+                else {
+                    setServerErr("Something is wrong. Please contact the server side administrator for details.");
+                }
             }
-        });
-        // console.log(movieResult, movieResult.status);
-        if (movieResult.status == 200) {
-            setUpdateResult("Movie record updated");
-        }
-        else {
-            if (movieResult.status == 400) {
-                var errorObject = await movieResult.json();
-                // console.log(errorObject);
-                if (errorObject.errors.Name) {
-                    setMovieNameErr("Movie Name is required.");
-                }
-                if (errorObject.errors.rating) {
-                    setRatingErr("Please rate between 1-10 for this movie.");
-                }
-                if (errorObject.errors.rentPrice) {
-                    setRentErr("Rent is required.");
-                }
-                if (errorObject.errors.Genre) {
-                    setGenreErr("Genre is required.");
-                }
+            else {
+                setUpdateResult("");
+                name == "" ? setMovieNameErr("Movie Name is a required field") : setMovieNameErr("");
+                rating == "" ? setRatingErr("Rating is a required field") : setRatingErr("");
+                genre == "" ? setGenreErr("Genre is a required field") : setGenreErr("");
+                rentPrice == "" ? setRentErr("Rent is a required field") : setRentErr("");
             }
         }
+        catch (err) {
+            if (err == "TypeError: Failed to fetch") {
+                setServerErr("Please ensure you have an internet connection.");
+            }
+            else {
+                setServerErr(`Error: ${err}`);
+            }
+        }
+
     }
 
     return (
         <div className="movie-login-background">
             <div className="movie container">
+                {
+                    serverErr && <div className="server-validation">
+                        <h3>{serverErr}</h3>
+                    </div>
+                }
                 {updateResult && <p className="update-success">{updateResult}</p>}
                 <h1>Update Movie</h1>
                 <div className="movie-form">
